4d7db90a6fa34808692085e23c8d76b0fce2d9d3
Hangjit Rai <hangjit.rai@outlook.com>
2020-08-18 15:24:40 +1000

    Using Require

---
+++ FunWithNode/hello.js
@@ -0,0 +1,15 @@
+console.log('hello world');
+
+var x = {
+  name: 'hi',
+  place: 'world',
+};
+
+console.log(x.name + ' ' + x.place);
+
+var msgs = require('./msgs');
+var msg = new msgs();
+console.log(msg.first);
+
+var logger = require('./logger');
+logger.log('this is from the logger');
