712a141c8327fe2dcb5fc7642e90c5b7d197c0d8
Hangjit Rai <hangjit.rai@outlook.com>
2020-08-19 16:34:18 +1000

    Added Theboard

---
+++ TheBoard/server.js
@@ -0,0 +1,26 @@
+var http = require("http");
+var express = require("express");
+var app = express();
+//var ejsEngine = require("ejs-locals");
+var controllers = require("./controllers");
+
+// Setup the View Engine
+//app.set("view engine", "jade");
+//app.engine("ejs", ejsEngine); // support master pages
+//app.set("view engine", "ejs"); // ejs view engine
+app.set("view engine", "vash");
+
+// set the public static resource folder
+app.use(express.static(__dirname + "/public"));
+
+// Map the routes
+controllers.init(app);
+
+app.get("/api/users", function (req, res) {
+  res.set("Content-Type", "application/json");
+  res.send({ name: "Shawn", isValid: true, group: "Admin" });
+});
+
+var server = http.createServer(app);
+
+server.listen(3000);
--- hello.js
+++
@@ -1,8 +0,0 @@
-console.log('hello world');
-
-var x = {
-  name: 'hi',
-  place: 'world',
-};
-
-console.log(x.name + ' ' + x.place);
