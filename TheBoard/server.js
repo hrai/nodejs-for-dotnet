ed6f2517cd096e43d545d6101e27f5a7ad924c4e
Hangjit Rai <hangjit.rai@outlook.com>
2020-08-19 16:37:41 +1000

    Cleanup

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -1,24 +1,24 @@
-var http = require("http");
-var express = require("express");
+var http = require('http');
+var express = require('express');
 var app = express();
 //var ejsEngine = require("ejs-locals");
-var controllers = require("./controllers");
+var controllers = require('./controllers');
 
 // Setup the View Engine
 //app.set("view engine", "jade");
 //app.engine("ejs", ejsEngine); // support master pages
 //app.set("view engine", "ejs"); // ejs view engine
-app.set("view engine", "vash");
+app.set('view engine', 'vash');
 
 // set the public static resource folder
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + '/public'));
 
 // Map the routes
 controllers.init(app);
 
-app.get("/api/users", function (req, res) {
-  res.set("Content-Type", "application/json");
-  res.send({ name: "Shawn", isValid: true, group: "Admin" });
+app.get('/api/users', function(req, res) {
+  res.set('Content-Type', 'application/json');
+  res.send({ name: 'Shawn', isValid: true, group: 'Admin' });
 });
 
 var server = http.createServer(app);
