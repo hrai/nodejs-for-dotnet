3aedc9bcb02c74e6935530198004777205036ea0
Hangjit Rai <hangjit.rai@outlook.com>
2020-08-25 11:05:23 +1000

    Updated Db File

---
+++ TheBoard/data/database.js
@@ -0,0 +1,23 @@
+(function(database) {
+  var mongodb = require('mongodb');
+  var mongoUrl = 'mongodb://localhost:27017/theBoard';
+  var theDb = null;
+
+  database.getDb = function(next) {
+    if (!theDb) {
+      mongodb.MongoClient.connect(mongoUrl, function(err, db) {
+        if (err) {
+          next(err, null);
+        } else {
+          theDb = {
+            db: db,
+          };
+
+          next(null, theDb);
+        }
+      });
+    } else {
+      next(null, theDb);
+    }
+  };
+});
